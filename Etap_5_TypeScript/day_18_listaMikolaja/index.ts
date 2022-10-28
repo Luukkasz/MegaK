adbb306efdd87e6d50bc5d8e6684e0a943a04d3e
l.machalewski <lukasz.machalewski@currency-one.com>
2022-10-28 16:39:31 +0200

    day-20

---
+++ Etap_5_TypeScript/day_18_listaMikolaja/index.ts
@@ -0,0 +1,38 @@
+const express = require('express');
+require('express-async-errors');
+const methodOverride = require('method-override');
+const { handleError } = require("./utils/error");
+const { homeRouter } = require("./routers/home");
+const { childRouter } = require("./routers/child");
+const { giftRouter } = require("./routers/gift");
+const hbs = require('express-handlebars');
+require('./utils/db');
+const {handlebarsHelpers} = require("./utils/handlebars-helpers");
+
+
+const app = express();
+
+// Middleware
+app.use(methodOverride('_method'));
+// Do obslugi formularzy
+app.use(express.urlencoded({
+    extended: true,
+}));
+// Wskazujemy ze uzywamy plikow statycznych z folderu public
+app.use(express.static('public'));
+// Parsowanie danych ktore przychodza w JSON
+app.use(express.json()); //Content-type: application/json
+app.engine('.hbs', hbs.engine({
+    extname: '.hbs',
+    helpers: handlebarsHelpers, // dodatkowe funkcjonalnosci, ktore chcemy dodac do Handlebarsow
+}));
+app.set('view engine', '.hbs');
+
+app.use('/', homeRouter);
+app.use('/child', childRouter)
+app.use('/gift', giftRouter)
+app.use(handleError);
+
+app.listen(3000, 'localhost', () => {
+    console.log('Program działa na adresie http://localhost:3000');
+})
---
+++ Etap_5_TypeScript/day_18_listaMikolaja/routers/gift.ts
@@ -0,0 +1,25 @@
+const {Router} = require('express');
+const {GiftRecord} = require("../records/gift-record");
+const giftRouter = Router();
+giftRouter
+    .get('/', async (req, res) => {
+        const giftsList = await GiftRecord.listAll();
+        res.render('gift/gift-list.hbs', {
+            giftsList,
+        });
+    })
+
+    .post('/', async (req, res) => {
+        const data = {
+            ...req.body,
+            count: Number(req.body.count),
+        };
+
+        const newGift = new GiftRecord(data);
+        await newGift.insert();
+
+        res.redirect('/gift');
+    })
+module.exports = {
+    giftRouter,
+}
