6d19a225b3a541124f97e18a12535971518d86fa
l.machalewski <lukasz.machalewski@currency-one.com>
2023-02-22 21:15:44 +0100

    etap_7_project: added connection to mysql database

--- Etap_7_Project/mega-ads-back/index.ts
+++ Etap_7_Project/mega-ads-back/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from 'cors'
 import 'express-async-errors'
+import {handleError, ValidationError} from "./utils/erros";
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(express.json())
+
+// Routes....
+
+app.get('/', async (req,res) => {
+    throw new ValidationError('Daamn!')
+})
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001')
