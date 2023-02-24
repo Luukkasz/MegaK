8ae2a362766886a919ab5aaa75bdfd509e6876fa
l.machalewski <lukasz.machalewski@currency-one.com>
2023-02-24 13:30:37 +0100

    etap_7_project: basic config of OpenStreetMaps in my app

--- Etap_7_Project/mega-ads-front/src/components/Map/Map.tsx
+++ Etap_7_Project/mega-ads-front/src/components/Map/Map.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import '../../utils/fix-map-icon'
+
 import './Map.css'
+import 'leaflet/dist/leaflet.css'
 
 export const Map = () => {
     return (
         <div className="map">
-            ...
+            <MapContainer center={[53.0107907,18.6147001]} zoom={13}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+                />
+
+                <Marker position={[53.0107907,18.6147001]}>
+                    <Popup>
+                        <h2>Moje miasto Toruń</h2>
+                        <p>Super miejsce :)</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     )
 }
