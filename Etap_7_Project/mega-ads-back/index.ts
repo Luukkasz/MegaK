b9e25f41395a0964f9274bd9d35b44287f25f046
l.machalewski <lukasz.machalewski@currency-one.com>
2023-09-20 23:33:30 +0200

    Added rate-limiter

--- Etap_7_Project/mega-ads-back/index.ts
+++ Etap_7_Project/mega-ads-back/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import cors from 'cors'
 import 'express-async-errors'
 import {handleError, ValidationError} from "./utils/erros";
+import { rateLimit } from 'express-rate-limit'
 
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(express.json())
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}))
 
 // Routes....
 
