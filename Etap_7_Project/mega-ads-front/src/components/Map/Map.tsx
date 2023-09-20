054ca921fb35384576914808542bb7ec0d7fdd23
l.machalewski <lukasz.machalewski@currency-one.com>
2023-09-20 23:58:32 +0200

    added context-api

--- Etap_7_Project/mega-ads-front/src/components/Map/Map.tsx
+++ Etap_7_Project/mega-ads-front/src/components/Map/Map.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import '../../utils/fix-map-icon'
 
 import './Map.css'
 import 'leaflet/dist/leaflet.css'
+import {SearchContext} from "../../contexts/search.context";
 
 export const Map = () => {
+    const {search} = useContext(SearchContext)
+
+    useEffect(() => {
+        console.log('Make request to search for', search)
+
+    }, [search])
+
     return (
         <div className="map">
+            <h1>Search for: {search}</h1>
             <MapContainer center={[53.0107907,18.6147001]} zoom={13}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- Etap_7_Project/mega-ads-front/src/components/layout/Header.tsx
+++ Etap_7_Project/mega-ads-front/src/components/layout/Header.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {SyntheticEvent, useContext, useState} from 'react';
 import './Header.css'
 import {Btn} from "../common/Btn";
+import {SearchContext} from "../../contexts/search.context";
 
-export const Header = () => (
-    <header>
-        <h1>
-            <strong>Mega </strong>Ogłoszenia
-        </h1>
-        <Btn text='Dodaj ogłoszenie'/>
-        <div className="search">
-            <input type="text"/>
-            <Btn text="Szukaj"/>
-        </div>
-    </header>
-)
+export const Header = () => {
+    const {search, setSearch} = useContext(SearchContext)
+    const [inputVal, setInputVal] = useState<string>(search)
+
+    const setSearchFromLocalState = (event: SyntheticEvent) => {
+        event.preventDefault()
+        setSearch(inputVal)
+    }
+
+
+    return (
+        <header>
+            <h1>
+                <strong>Mega </strong>Ogłoszenia
+            </h1>
+            <Btn text='Dodaj ogłoszenie'/>
+            <form className="search" onSubmit={setSearchFromLocalState}>
+                <input
+                    type="text"
+                    value={inputVal}
+                    onChange={e => setInputVal(e.target.value)} />
+                <Btn text="Szukaj"/>
+            </form>
+        </header>
+    )
+}
+
